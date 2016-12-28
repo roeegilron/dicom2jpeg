d64df2fd0a6c7eb80146b0fb0afb6434d01c4092
roeegilron <roeegilron@gmail.com>
2016-12-27 21:47:20 -0800

    better design
    still sucks...

--- DicomToJPEGconverter.m
+++ DicomToJPEGconverter.m
@@ -4,6 +4,8 @@ function DicomToJPEGconverter()
 % and converts them to a mirrored directory structure with jpegs in them.
 % * note this this relies on directory being only one "deep".
 
+% set params:
+params.flatoutput = 1; %output all jpegs to bnc dir;
 dirchosen = uigetdir(pwd,'Choose a directory with directories of dicoms');
 dirsfound = findFilesBVQX(dirchosen,'*',struct('dirs',1));
 bncdir = fullfile(pwd,'jpegs');
@@ -15,16 +17,25 @@ for d = 1:length(dirsfound)
         if exist(ffls{f},'file');
             [pn,fn] = fileparts(ffls{f});
             [~,pn] = fileparts(pn);
-            outdir = fullfile(bncdir,pn);
-            if f == 1 % only create dir once 
-                mkdir(outdir);
-            end
+            % read and plot file 
             [x, map] = dicomread(ffls{f});
             hfig = figure('visible','off');
             im = imagesc(x);
             colormap('gray');
             axis off
-            fnsave = fullfile(outdir,[fn, '.jpeg']);
+            
+            % do annoying saving and printing 
+            if params.flatoutput
+                [s,UUID] = system('uuidgen'); % create unique id since dicoms named the same
+                fnmuse = sprintf('XRAY-%.2d_%s_%s.jpeg',d,UUID,fn);
+                fnsave = fullfile(bncdir,fnmuse);
+            else
+                outdir = fullfile(bncdir,pn);
+                if f == 1 % only create dir once
+                    mkdir(outdir);
+                end
+                fnsave = fullfile(outdir,[fn, '.jpeg']);
+            end
             saveas(hfig, fullfile(fnsave));
         end
     end
